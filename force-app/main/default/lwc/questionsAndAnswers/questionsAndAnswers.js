67ca4a8b9da134e58541bb459374e24e24be385e
kevin-xhk <kevinxhakaj@gmail.com>
2022-10-26 15:07:23 +0200

    added mock json to qna lwc

--- force-app/main/default/lwc/questionsAndAnswers/questionsAndAnswers.js
+++ force-app/main/default/lwc/questionsAndAnswers/questionsAndAnswers.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
 
-export default class QuestionsAndAnswers extends LightningElement {}
+export default class QuestionsAndAnswers extends LightningElement {
+
+    quiz = {
+        'id': 1,
+        'description' : "who's the tallest us president?",
+        'answers' : [
+            {
+                'id': 1,
+                'description':"Abraham Lincoln",
+                'isCorrect': true
+            },
+            {
+                'id': 2,
+                'description':"Barack Obama",
+                'isCorrect': false
+            },            
+            {
+                'id': 1,
+                'description':"Theodore Roosevelt",
+                'isCorrect': false
+            },            
+            {
+                'id': 1,
+                'description':"Thomas Jefferson",
+                'isCorrect': false
+            },
+        ]
+    }
+}

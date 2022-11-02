494db4075c2b9017df075e61bdc2a80c7903e4cd
kevin-xhk <kevinxhakaj@gmail.com>
2022-11-02 11:19:05 +0100

    made answers dynamic, added ui elements

--- force-app/main/default/lwc/questionsAndAnswersMiger/questionsAndAnswersMiger.js
+++ force-app/main/default/lwc/questionsAndAnswersMiger/questionsAndAnswersMiger.js
@@ -2,42 +2,47 @@
  * Created by mshkrepa on 10/26/2022.
  */
 
-import {LightningElement, track} from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getQuizQuestion from '@salesforce/apex/QuestionsAndAnswersController.getQuizQuestion';
+import { publish, MessageContext } from 'lightning/messageService';
+import questionMC from '@salesforce/messageChannel/QuestionMC__c';
 
-export default class QuestionsAndAnswersMiger extends LightningElement {
 
+export default class QuestionsAndAnswers extends LightningElement {
+
+    // question, answers and buttons data
     @track question;
-    @track firstAnswer;
-    @track secondAnswer;
-    @track thirdAnswer;
-    @track fourthAnswer;
+    @track answers;
     @track selectedAnswer;
-    @track firstAnswerValue;
-    @track secondAnswerValue;
-    @track thirdAnswerValue;
-    @track fourthAnswerValue;
+
+    // LMS publish vars
+    @wire(MessageContext) messageContext;
     @track selectedAnswer;
 
     connectedCallback(){
+        // populate vars for data display
         getQuizQuestion()
             .then(data=>{
                 this.question=data.Name;
-                this.firstAnswer=data.Quiz_Answers__r[0].Name;
-                this.firstAnswerValue=data.Quiz_Answers__r[0].Id;
-                this.secondAnswer=data.Quiz_Answers__r[1].Name;
-                this.secondAnswerValue=data.Quiz_Answers__r[1].Id;
-                this.thirdAnswer=data.Quiz_Answers__r[2].Name;
-                this.thirdAnswerValue=data.Quiz_Answers__r[2].Id;
-                this.fourthAnswer=data.Quiz_Answers__r[3].Name;
-                this.fourthAnswerValue=data.Quiz_Answers__r[3].Id;
+                this.answers=data.Quiz_Answers__r;
             }).catch(error => {
             console.log("ERROR / imperative apex call / getQuizQuestion: " + JSON.stringify(error))
         });
     }
 
+    // get Id of selected answer
     selectedAnswerEvent(event){
+        event.target.className='slds-button slds-button_brand slds-button_stretch slds-col slds-size_5-of-12';
         this.selectedAnswer=event.target.value;
     }
 
+    // send selected answer's Id to verify-lwc
+    handleVerifyClick(){
+        console.log('qna.handleVerifyClick start')
+        // take selected answer's id val here
+        const payload = {selectedAnswerId: this.selectedAnswer} //testing purposes only
+        console.log('payload: ' + JSON.stringify(payload))
+        // publish it to QuestionMessageChannel
+        publish(this.messageContext, questionMC, payload);
+    }
 }
